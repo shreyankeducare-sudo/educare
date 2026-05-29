f308c8a9596d0766c7ba8bb28d4e45903561510f
shreyankeducare-sudo <shreyankeducare@gmail.com>
2026-05-29 21:06:47 +0530

    ADD: gtm code

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ import Footer from "../components/Footer";
 import WhatsAppFloating from "../components/WhatsAppFloating";
 import { JsonLd, getOrganizationSchema } from "../components/SchemaMarkup";
 
-import Script from "next/script";
-
 const bricolageGrotesque = Bricolage_Grotesque({
   variable: "--font-bricolage",
   subsets: ["latin"],
@@ -42,6 +40,19 @@ export default function RootLayout({
       lang="en"
       className={`${bricolageGrotesque.variable} ${montserrat.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MWRK22PJ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="min-h-full flex flex-col pt-20">
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -53,14 +64,6 @@ export default function RootLayout({
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MWRK22PJ');`}
-        </Script>
 
         <JsonLd schema={getOrganizationSchema()} />
         <Header />
